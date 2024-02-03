b7939d8051f7c325d19b005de6defdb53425406c
zulfiqar-usama <usamaz39@gmail.com>
2024-02-03 17:19:09 +0000

    Able to run on more than one computer on the same wifi network

--- app.py
+++ app.py
@@ -15,5 +15,5 @@ def home():
                   <input type="submit" value="Execute" /><br>
               </form>'''
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__  == "__main__":
+    app.run(host='0.0.0.0', debug=True)
